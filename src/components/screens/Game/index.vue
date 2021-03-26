23ae7bd6a84fed56a1e161ebf4fab481b4bc7be4
Jakub Niewczas <jniewczas@degica.com>
2021-03-26 13:39:29 +0900

    add moving obstacles

--- src/components/screens/Game/index.vue
+++ src/components/screens/Game/index.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="game" @click="click">
     <img class="player" :src="dinoPng" :style="{ top: `${dinoY}px` }" />
+    <img
+      v-for="obstacle in obstacles"
+      :key="obstacle.id"
+      class="obstacle"
+      :src="obstaclePng"
+      :style="{ left: `${obstacle.x}px`, top: `${obstacle.y}px` }"
+    />
   </div>
 </template>
 
 <script>
 import { HapticsImpactStyle, Plugins } from "@capacitor/core";
 import dinoPng from "../../../assets/images/dino.png";
+import obstaclePng from "../../../assets/images/obstacle.png";
 
 const { Haptics } = Plugins;
 export default {
   data() {
     return {
       dinoPng,
+      obstaclePng,
       dinoY: 350,
       dinoYVelocity: 0,
-      force: -0.3,
+      force: -0.5,
       jumpForce: 6,
+      obstacles: [
+        { id: 1, x: 414, y: -250 },
+        { id: 2, x: 2 * 300, y: -250 },
+        { id: 3, x: 3 * 300, y: -0 },
+        { id: 4, x: 4 * 300, y: -150 },
+      ],
+      obstacleYVelocity: 3,
       gameInterval: null,
     };
   },
@@ -30,14 +46,28 @@ export default {
   },
 
   methods: {
+    click() {
+      Haptics.impact({ style: HapticsImpactStyle.Heavy });
+      this.dinoYVelocity = this.jumpForce;
+    },
+
     gameLoop() {
+      this.adjustDinoVelocity();
+      this.moveAndRemoveObstacles();
+    },
+
+    adjustDinoVelocity() {
       this.dinoYVelocity += this.force;
       this.dinoY -= this.dinoYVelocity;
     },
 
-    click() {
-      Haptics.impact({ style: HapticsImpactStyle.Heavy });
-      this.dinoYVelocity = this.jumpForce;
+    moveAndRemoveObstacles() {
+      this.obstacles = this.obstacles
+        .map((obstacle) => ({
+          ...obstacle,
+          x: obstacle.x - this.obstacleYVelocity,
+        }))
+        .filter((obstacle) => obstacle.x > -60);
     },
   },
 };
@@ -55,5 +85,10 @@ export default {
   left: 50%;
   width: 60px;
   transform: translate(-50%, -50%);
+}
+
+.obstacle {
+  position: absolute;
+  width: 60px;
 }
 </style>
